655c0d88e7739ecbfbb32f4b656645037c75673d
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-04-04 00:55:11 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,11 +6,5 @@ describe 'new post' do
     expect(page.status_code).to eq(200)
   end
   
-describe 'new post' do
- 
-  ...
- 
-  it 'renders HTML in the /new template' do
-    visit new_post_path
-    expect(page).to have_content('Post Form')
+
 end
